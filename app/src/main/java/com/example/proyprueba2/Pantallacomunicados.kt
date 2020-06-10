0699a78c85acfb81d97a7ca28d8d2109c3000c5c
migueladrr17 <migueladr17@gmail.com>
2020-06-10 01:04:36 -0500

    Se agregó la pantalla de comunicados

---
+++ app/src/main/java/com/example/proyprueba2/Pantallacomunicados.kt
@@ -0,0 +1,11 @@
+package com.example.proyprueba2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantallacomunicados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantallacomunicados)
+    }
+}
