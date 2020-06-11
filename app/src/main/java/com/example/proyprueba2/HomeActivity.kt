b97d4beac34f0915ac1e9f9cbc4facb1b05ada6c
jack.daga <3Nc0ntr4d05_9>
2020-06-10 19:38:08 -0500

    Registrar y Acceder con correo: OK Registrar y Acceder con Google: In Progress He vuelto a agregar el splashscreen.xml

--- app/src/main/java/com/example/proyprueba2/HomeActivity.kt
+++ app/src/main/java/com/example/proyprueba2/HomeActivity.kt
@@ -1,12 +1,12 @@
 package com.example.proyprueba2
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
-enum class ProviderType{
+enum class ProviderType {
     // Autenticación básica: email y contraseña
     BASIC,
     GOOGLE
@@ -16,30 +16,40 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        //Se ingresa comentario de prueba
+
         // Setup
         val bundle = intent.extras
-        val email=bundle?.getString("email")
-        val provider =bundle?.getString("provider")
-        setup(email?:"",provider?:"")
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
 
         // Guardar el Proveedor y el usuario autenticado
-        val prefs = getSharedPreferences(getString(R.Strings.prefs_file), Context.MODE_PRIVATE).edit()
-        prefs.putString("email",email)
-        prefs.putString("provider",provider)
+
+        val prefs =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        // Guardar una cadena de texto
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
         prefs.apply()
 
     }
 
-    private fun setup(email:String, provider:String){
+    private fun setup(email: String, provider: String) {
         //
         title = "Inicio"
         // Agregamos los valores a los id de los layouts
         emailTextView.text = email
-        providerTextView.text=provider
+        providerTextView.text = provider
 
         // Uso del botón "Cerrar Sesión"
         logOutButton.setOnClickListener {
+
+            // Borrado de datos
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             // Llamar a FBase
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
