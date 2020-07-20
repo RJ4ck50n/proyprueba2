eca211a319dc0ad7bd48b0b2f6f73413b05e5351
migueladrr17 <migueladr17@gmail.com>
2020-07-20 18:51:49 -0500

    Se agregó la pantalla de Supermercados.

--- app/src/main/java/com/example/proyprueba2/SupermercadosActivity.kt
+++ app/src/main/java/com/example/proyprueba2/SupermercadosActivity.kt
@@ -3,9 +3,9 @@ package com.example.proyprueba2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SupermercadosActivity : AppCompatActivity() {
+class PantallasupermercadosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_supermercados)
+        setContentView(R.layout.pantallasupermercados)
     }
 }
