73567b081b2c56723fbd65deffc3a0e5bd8823e0
migueladrr17 <migueladr17@gmail.com>
2020-08-17 04:43:10 -0500

    Se logra obtener los valores para el spinner pero con cantidad Límite

---
+++ app/src/main/java/com/example/proyprueba2/Producto.java
@@ -0,0 +1,77 @@
+package com.example.proyprueba2;
+
+public class Producto {
+    private String Marca;
+    private String Producto;
+    private String Nombre;
+    private String Supermercado;
+    private Integer Unidades;
+    private Double Precio;
+    private String Peso;
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    private String Tipo;
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getProducto() {
+        return Producto;
+    }
+
+    public void setProducto(String producto) {
+        Producto = producto;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getSupermercado() {
+        return Supermercado;
+    }
+
+    public void setSupermercado(String supermercado) {
+        Supermercado = supermercado;
+    }
+
+    public Integer getUnidades() {
+        return Unidades;
+    }
+
+    public void setUnidades(Integer unidades) {
+        Unidades = unidades;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double precio) {
+        Precio = precio;
+    }
+
+    public String getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(String peso) {
+        Peso = peso;
+    }
+}
--- app/src/main/java/com/example/proyprueba2/fragment/ComparaFragment.kt
+++ app/src/main/java/com/example/proyprueba2/fragment/ComparaFragment.kt
@@ -2,44 +2,86 @@ package com.example.proyprueba2.fragment
 import ExpandableFaqsListAdapter
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ExpandableListView
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
+import android.widget.*
 import androidx.fragment.app.Fragment
+import com.example.proyprueba2.Producto
 import com.example.proyprueba2.R
-import com.example.proyprueba2.fragment.Faq
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ComparaFragment : Fragment() {
-    lateinit var faqsListView: ExpandableListView
-    lateinit var listAdapter: ExpandableFaqsListAdapter
     lateinit var faqsView: View
+    private lateinit var mibase: FirebaseDatabase
+    private lateinit var miReferencia: DatabaseReference
+    lateinit var listaProductos: ArrayList<String>
+    lateinit var adaptador: ArrayAdapter<String>
+    lateinit var spinnerProdarr: Spinner
+    lateinit var spinnerPrueba: Spinner
+    lateinit var listaArroz: ArrayList<String>
+    lateinit var listaLeche: ArrayList<String>
 
-
+    
     @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        faqsView = inflater.inflate(R.layout.fragment_compara, container, false)
+        //faqsListView = faqsView?.findViewById(R.id.rvFaqs) as ExpandableListView
+        spinnerProdarr= faqsView.findViewById(R.id.spinnerPrueba)
+        spinnerPrueba= faqsView.findViewById(R.id.spinnerLeche)
+        mibase = FirebaseDatabase.getInstance()
+        miReferencia = Firebase.database.reference
+        listaLeche=ArrayList()
+        listaArroz=ArrayList()
 
-        faqsView = inflater?.inflate(R.layout.fragment_compara, container, false)
+        miReferencia.child("Arroz").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                listaArroz.clear()
+                for(snapshot in dataSnapshot.children){
+                    val unProducto=snapshot.getValue(Producto::class.java)
+                    val nombre= unProducto?.tipo + " " + unProducto?.marca
+                    listaArroz.add(nombre)
+                    Log.e("Producto Final Arroz: ", ""+nombre)
+                    val arrayAdapters = ArrayAdapter(requireContext(),R.layout.support_simple_spinner_dropdown_item,listaArroz)
+                    spinnerProdarr.adapter = arrayAdapters
+                    arrayAdapters.notifyDataSetChanged()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        })
 
-        faqsListView = faqsView?.findViewById(R.id.rvFaqs) as ExpandableListView
+        miReferencia.child("Leche").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                listaLeche.clear()
+                for(snapshot in dataSnapshot.children){
+                    val unProducto=snapshot.getValue(Producto::class.java)
+                    val nombre= unProducto?.nombre
+                    val marca=unProducto?.marca
+                    val peso=unProducto?.peso
+                    if (nombre != null) {
+                        listaLeche.add(nombre)
+                    }
+                    val arrayAdapter = ArrayAdapter(requireContext(),R.layout.support_simple_spinner_dropdown_item,listaLeche)
+                    spinnerPrueba.adapter = arrayAdapter
+                    arrayAdapter.notifyDataSetChanged()
+                    Log.e("Producto Final: ", ""+nombre)
+                }
 
-        val faqsListParent= listOf("Numbers","Fruit")
-        val fruitsList=listOf("Apple","Orange","Banana")
-        val madera=listOf("Miguel","Angel","David")
-        val listchild= HashMap<String, List<String>>()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                // ...
+            }
+        })
 
-        listchild.put(faqsListParent[0], madera)
-        listchild.put(faqsListParent[1], fruitsList)
-
-        listAdapter = ExpandableFaqsListAdapter(activity!!, faqsListParent,listchild )
-        faqsListView.setAdapter(listAdapter)
-
-// If you want to change parent item icon (up/down/expanded/collapse)
-// effect. Add onGroupClick Listener like below.
-
+        /*
         faqsListView.setOnGroupClickListener { expandableListView, view, i, l ->
             if (expandableListView.isGroupExpanded(i)) {
                 //var imgView = view.findViewById<ImageView>(R.id.imgIonNextFaq)
@@ -50,13 +92,12 @@ class ComparaFragment : Fragment() {
             }
             return@setOnGroupClickListener false
         }
-
-
-        return faqsView
+         */
+       return faqsView
     }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 }
+
